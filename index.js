ec4e6ee0fc271b98808530e5239b9cfc37ad7b19
Nick Comer <nick@comer.io>
2013-08-10 13:21:39 -0400

    set production configuration

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 var io = require('socket.io').listen(9000);
 
-io.sockets.on('connection', function(socket){
-	socket.on('join', function(room){
-		if(room === undefined) return;
+io.enable('browser client etag');
+io.set('log level', 1);
+
+io.set('transports', [
+	'websocket', 'flashsocket', 'htmlfile', 'xhr-polling', 'jsonp-polling'
+]);
+
+io.sockets.on('connection', function(socket) {
+	socket.on('join', function(room) {
+		if (room === undefined) return;
 
 		socket.join(room);
 	});
 
-	socket.on('msg', function(data){
-		if(data === undefined) return;
-		if(data.room === undefined) return;
-		if(data.msg === undefined) return; 
+	socket.on('msg', function(data) {
+		if (data === undefined) return;
+		if (data.room === undefined) return;
+		if (data.msg === undefined) return;
 
 		socket.broadcast.to(data.room).emit('msg', data.msg);
 	});
